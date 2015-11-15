b5809ea58676eccd6640d20403cf1638c4b15463
ch-alekhya <alekhyacheruvu@gmail.com>
2015-11-15 11:05:50 -0700

    changes invilves checking "syn" meanings in API

--- src/kparser/JsonReader.java
+++ src/kparser/JsonReader.java
@@ -8,6 +8,7 @@ import java.io.Reader;
 
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.util.List;
 
 import org.json.simple.parser.ParseException;
 import org.json.simple.parser.JSONParser;
@@ -17,7 +18,10 @@ import org.json.*;
 public class JsonReader {
 
 	private static String readAll(Reader rd) throws IOException {
-		StringBuilder sb = new StringBuilder();   /* readall builds the contents of the strings in the buffer */
+		StringBuilder sb = new StringBuilder(); /*
+												 * readall builds the contents
+												 * of the strings in the buffer
+												 */
 		int cp;
 		while ((cp = rd.read()) != -1) {
 			sb.append((char) cp);
@@ -27,11 +31,18 @@ public class JsonReader {
 
 	public static JSONObject readJsonFromUrl(String url) throws IOException,
 			JSONException {
-		InputStream is = new URL(url).openStream();    /* takes the input from the url and stores them in string format */
+		InputStream is = new URL(url).openStream(); /*
+													 * takes the input from the
+													 * url and stores them in
+													 * string format
+													 */
 		try {
 			BufferedReader rd = new BufferedReader(new InputStreamReader(is,
 					Charset.forName("UTF-8")));
-			String jsonText = readAll(rd);              /* the string buffer is given as input to the JSON object */
+			String jsonText = readAll(rd); /*
+											 * the string buffer is given as
+											 * input to the JSON object
+											 */
 			JSONObject json = new JSONObject(jsonText);
 			return json;
 		} finally {
@@ -40,55 +51,88 @@ public class JsonReader {
 
 	}
 
-	public String translate(String input, String pos) throws IOException, JSONException,
-			ParseException {
-		String result = input;
+	public String translate(String input, String pos) throws IOException,
+			JSONException, ParseException {
+		String main_result = input;
+		String result;
+		json json_sub = new json();
+		
+		pos=json_sub.posTagConvertor(pos);
+		System.out.print(input + " ");
+		System.out.println(pos);
 		try {
-			JSONObject json = readJsonFromUrl("https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20151025T082334Z.d1c51ebb15e03fc9.b9f5b9c541bd619e3a4fe3ffbb2823fd23fa2c81&lang=en-de&text="
+			JSONObject json_main = readJsonFromUrl("https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20151025T082334Z.d1c51ebb15e03fc9.b9f5b9c541bd619e3a4fe3ffbb2823fd23fa2c81&lang=en-de&text="
 					+ input);
-			String s = (json.get("def")).toString(); /* extracts the json object with respect to "def" variable */
 
-			JSONParser parser = new JSONParser();
-			Object obj = parser.parse(s);          /* parsers the array in JsonArray format */
-			JSONArray array = (JSONArray) obj;
-
-			s = (array.get(0)).toString();        /* gets the string format of the array */
-			json = new JSONObject(s);             /* returns the string as JSON object */
-
-			s = (json.get("tr")).toString();      /* gets the json array refer to "tr" variable */
-			obj = parser.parse(s);
-			array = (JSONArray) obj;             /* parses the array to get the JSON object */
-
-			s = (array.get(0)).toString();
-
-			json = new JSONObject(s);
-			result = (json.get("text")).toString();  /* gets the "text content of the translated word */
-			// System.out.println(json.get("text"));
-
-			/*
-			 * try{
-			 * 
-			 * System.out.println(json.get("syn")); } catch (JSONException e) {
-			 * 
-			 * }
-			 */
-			
-			  try{
-			  
-			 String german_pos=(json.getString("pos")).toString();  /* getting the POS tag o fthe transalated word */
-			 	if(german_pos==pos)
-			 		return result;
-			  } 
-			  
-			  catch (JSONException e) {
-			  
+			String s = json_sub.subString(json_main, "def");
+			Object obj = null;
+			JSONArray arr = json_sub.subArray(obj, s);
+			for (int i = 0; i < arr.size(); i++) {
+				org.json.simple.JSONObject sub = (org.json.simple.JSONObject) arr
+						.get(i);
+				
+				String res_pos = (String) sub.get("pos");
+				result = (String) sub.get("ts");
+				if (res_pos.equals(pos) && res_pos.equals("noun")) {
+					if (!result.isEmpty())
+					{
+						return result;
+					}
+				}
+				s = json_sub.subString_2(sub, "tr");
+				JSONArray arr1 = json_sub.subArray(obj, s);
+				json_main = new JSONObject(
+						json_sub.subStringFromsubArray(arr1));
+				res_pos = (String) json_main.get("pos");
+				String result_1 = (String) json_main.get("text");
+				boolean flag=false;
+			    String res_pos_tr=res_pos;
+				
+				if(res_pos.equals(pos) && !(result_1.isEmpty()))
+				{
+					
+					return result_1 ;
+				}
+				else if(!(result_1.isEmpty()))
+				{
+					flag=true;
+				}
+				s = json_sub.subString(json_main, "syn");
+				arr1 = json_sub.subArray(obj, s);
+				json_main = new JSONObject(
+						json_sub.subStringFromsubArray(arr1));
+				String result_2 = (String) json_main.get("text");
+				res_pos = (String) json_main.getString("pos");
+				
+				System.out.println(result_2);
+				
+				if(res_pos.equals(pos) && !(result_2.isEmpty()))
+				{
+					return result_2 ;
+				}
+				else if(!(result_2.isEmpty()))
+				{
+					System.out.println("pos tag in syn is " +res_pos_tr);
+					return result_2;
+				}
+				
+			  if(flag)
+			  {
+				 System.out.println("pos tag in tr " +res_pos);
+				return result_1;
 			  }
-			 
-		} catch (Exception e) {
-			// TODO Log such words for later phase
+			  else
+			  {
+				return input;  
+			  }
+			 }
+			
+
+		} /* end of try catch buffer */
+
+
+		 catch (Exception e) {
 		}
-
-		return result;
+		return main_result;
 	}
-
 }
