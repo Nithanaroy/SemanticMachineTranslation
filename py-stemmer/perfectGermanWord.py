aa2895ff555d28f5a0a331ffee662777a0ff5e06
krkreddy <ramakrishna.khpr@gmail.com>
2015-11-18 22:50:21 -0700

    Tense Mapping
    Added function to map the tenses between verbix.com and UPenn

--- py-stemmer/perfectGermanWord.py
+++ py-stemmer/perfectGermanWord.py
@@ -5,13 +5,23 @@ import urllib
 import re
 from collections import Counter
 
+# Function to map input tense with the tense presen tin verbix.com
+def tenseMapping(inTense):
+    if(inTense == 'VB' or inTense == 'VBG' or inTense == 'VBP' or inTense == 'VBZ'):
+        return 'present'
+    elif(inTense == 'VBD' or inTense == 'VBN'):
+        return 'past'
+    else:
+        return 'present'
 
-#Function to pick up the right frequent word
+
+# Function to pick up the right frequent word (e.g sein####present)
 def perfectWord(text):
     #Extracting base word and tense from text provided
     pos = text.find('####')
     base_word = text[:pos]
-    tense = text[pos+4:]
+    inTense = text[pos+4:]
+    tense = tenseMapping(inTense)
 
     #Variable declarations
     list_tenses = ['present','perfect','past','pluperfect','future']
@@ -37,4 +47,4 @@ def perfectWord(text):
     return group.most_common(1)[0][0]
 
 #Print all the acquired data
-print perfectWord('sein####perfect')
+print perfectWord('sein####VBN')
