84ba1367c8596fd89bbde607aa93ec8176954e5c
Nitin Pasumarthy <nithanaroy@gmail.com>
2015-11-18 20:23:52 -0700

    Added DEBUG Prints for JSON Reader

--- src/translator/json.java
+++ src/translator/json.java
@@ -14,107 +14,92 @@ import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONValue;
 import org.json.simple.parser.*;
+
+import utils.Constants;
 //import java.util.*;
 //import org.json.simple.parser.*;
 
+class json {
 
-class json{
-	
-	public String subString (org.json.JSONObject json_main, String str) throws JSONException 
-	{
+	public String subString(org.json.JSONObject json_main, String str) throws JSONException {
 		return (json_main.get(str)).toString();
 	}
-	
-	public String subString_2 (JSONObject json_main, String str) throws JSONException 
-	{
+
+	public String subString_2(JSONObject json_main, String str) throws JSONException {
 		return (json_main.get(str)).toString();
 	}
- 
-	public JSONArray subArray(Object obj, String str) throws ParseException
-	{
+
+	public JSONArray subArray(Object obj, String str) throws ParseException {
 		JSONParser parser = new JSONParser();
-		 obj = parser.parse(str);
+		obj = parser.parse(str);
 		return (JSONArray) obj;
 	}
-	
-	public String subStringFromsubArray(JSONArray arr)
-	{
-		String s=(arr.get(0)).toString();
+
+	public String subStringFromsubArray(JSONArray arr) {
+		String s = (arr.get(0)).toString();
 		return s;
 	}
-	
-	public String posTagConvertor(String str)
-	{
-		
-		if(str.equals(null))
-		{
+
+	public String posTagConvertor(String str) {
+
+		if (str == null) {
 			return "null";
 		}
-		char c= str.charAt(0);
-		try{
-			switch(c)
-			{
-				case 'V' : 
-					{
-						str="verb";
-						break;
-					}
-				case 'N':
-					{
-						str="noun";
-						break;
-					}
-				case 'R':
-				{
-					str="adverb";
-					break;
-				}
-				case 'J':
-				{
-					str="adjective";
-					break;
-				}
-				case 'D':
-				{
-					str="determiner";
-					break;
-				}
-				case 'W':
-				case 'P':
-				{
-					str="pronoun";
-					break;
-				}
-				case 'I':
-				{	
-					str="preposition";
-					break;
-				}
-				case 'C':
-				{
-					str="conjunction";
-					break;
-				}
-				case 'U':
-				{
-					str="interjunction";
-					break;
-				}
-				default:
-				{
-					str="null";
-					break;
-				}
-			
+		char c = str.charAt(0);
+		try {
+			switch (c) {
+			case 'V': {
+				str = "verb";
+				break;
 			}
-		}
-		catch (Exception e)
-		{
-			System.out.println("pos is null");
+			case 'N': {
+				str = "noun";
+				break;
+			}
+			case 'R': {
+				str = "adverb";
+				break;
+			}
+			case 'J': {
+				str = "adjective";
+				break;
+			}
+			case 'D': {
+				str = "determiner";
+				break;
+			}
+			case 'W':
+			case 'P': {
+				str = "pronoun";
+				break;
+			}
+			case 'I': {
+				str = "preposition";
+				break;
+			}
+			case 'C': {
+				str = "conjunction";
+				break;
+			}
+			case 'U': {
+				str = "interjunction";
+				break;
+			}
+			default: {
+				str = "null";
+				break;
+			}
+
+			}
+		} catch (Exception e) {
+			if (Constants.DEBUG) {
+				System.out.println("pos is null");
+			}
+
 			return null;
 		}
-		
+
 		return str;
 	}
-	
+
 }
--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -3,4 +3,6 @@ package utils;
 public class Constants {
 	public static final String rawSetencesFile = "data/raw.txt";
 	public static final String correctSetencesFile = "data/correct.txt";
+
+	public static final boolean DEBUG = false;
 }
