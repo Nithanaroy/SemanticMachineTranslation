72adfbda6a6a350788a2401eefb3c6cc17d5b26a
Sujata <sujatasridharan.93@gmail.com>
2015-11-29 14:44:22 -0800

    modified aligner

--- py-aligner/align.py
+++ py-aligner/align.py
@@ -4,28 +4,32 @@ from nltk.corpus.util import LazyCorpusLoader
 from nltk.corpus.europarl_raw import german, english
 from nltk.corpus.reader import AlignedCorpusReader
 from nltk.translate import AlignedSent, Alignment
+from nltk.corpus.reader.plaintext import PlaintextCorpusReader
 
-def decode(filename):
-	comtrans = LazyCorpusLoader(
-   		'comtrans', AlignedCorpusReader, r'(?!\.).*\.txt',
-     		encoding='iso-8859-1')
-	files = filename.split('(')
-	
+
+def align(filename):
+
+	files = filename.split('(') 
+	with open(files[0]) as f:
+		with open("/newcorpus/source.txt","w") as f1:
+			for line in f:
+				f1.write(line)
+
+	corpusdir = './newcorpus/'
+	newcorpus = PlaintextCorpusReader(corpusdir, '.*')
 	out = open(files[1], "w")
 	i = 0
 	temp =[]
-	while i<20:
-		temp.append(comtrans.aligned_sents(files[0])[i].words)
-		tempVal = str(temp[i])
-		tempVal = tempVal.replace(",", "")
-		tempVal = tempVal.replace("u'", "")
-		tempVal = tempVal.replace("'", "")
-		tempVal = tempVal.replace("[", "")
-		tempVal = tempVal.replace("]", "")
-		out.write(tempVal+os.linesep)
-		i += 1
+	temp.append(newcorpus.sents(('source.txt')))
+	tempVal = str(temp[i])
+	tempVal = tempVal.replace(",", "")
+	tempVal = tempVal.replace("u'", "")
+	tempVal = tempVal.replace("'", "")
+	tempVal = tempVal.replace("[", "")
+	tempVal = tempVal.replace("]", "")
+	out.write(tempVal+os.linesep)
 	out.close()
-	return files[1]
+	return
 
 
 
