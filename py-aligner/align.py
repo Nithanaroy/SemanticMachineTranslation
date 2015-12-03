5d34293f2191de22ae2d51514d1dd74745afbcb9
Sujata <sujatasridharan.93@gmail.com>
2015-12-03 13:21:30 -0800

    final align

--- py-aligner/align.py
+++ py-aligner/align.py
@@ -9,18 +9,21 @@ from nltk.corpus.reader.plaintext import PlaintextCorpusReader
 
 def align(filename):
 
-	files = filename.split('(') 
+	files = filename.split('(')
+	ripe_file = os.path.abspath(files[1])
+	raw_file = os.path.abspath(files[0])
+	raw_for_nltk = os.path.abspath('../newcorpus/source.txt')
 	with open(files[0]) as f:
-		with open("/newcorpus/source.txt","w") as f1:
+		with open(raw_for_nltk,"w") as f1:
 			for line in f:
 				f1.write(line)
 
-	corpusdir = './newcorpus/'
-	newcorpus = PlaintextCorpusReader(corpusdir, '.*')
-	out = open(files[1], "w")
+	corpusdir = '../newcorpus/'
+	newcorpus = PlaintextCorpusReader(corpusdir, '.*',sent_tokenizer=nltk.data.LazyLoader('tokenizers/punkt/german.pickle'))
+	out = open(ripe_file, "w")
 	i = 0
 	temp =[]
-	temp.append(newcorpus.sents(('source.txt')))
+	temp.append(newcorpus.sents(raw_for_nltk))
 	tempVal = str(temp[i])
 	tempVal = tempVal.replace(",", "")
 	tempVal = tempVal.replace("u'", "")
@@ -30,6 +33,5 @@ def align(filename):
 	out.write(tempVal+os.linesep)
 	out.close()
 	return
-
 
 
